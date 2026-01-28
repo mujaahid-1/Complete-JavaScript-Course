6369ec0b74087289db593538a5036001ef0c532e
Mujaahid <hackm680@gmail.com>
2026-01-28 16:27:58 +0300

    updated both hello and values and variables

--- 01-Fundamentals-Part-1/2. Values and Variables/script.js
+++ 01-Fundamentals-Part-1/2. Values and Variables/script.js
@@ -1,7 +1,7 @@
-alert("Hello World");
-
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is fun!!");
+if (js === "amazing") {
+  alert("JavaScript is fun!!");
+}
 
 let myName = "Mujaahid";
 console.log(myName);
