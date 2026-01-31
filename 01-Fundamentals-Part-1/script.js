9c6e9ce5de573c167a95060284ca2b9df05888c1
Mujaahid <hackm680@gmail.com>
2026-01-31 07:16:01 +0300

    Variables and Values

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,1 +1,19 @@
+// *** Printing hello world ***
 console.log("Hello World");
+
+// *** Variables and values ***
+let myName = "Mujaahid";
+// Variable   // Value
+
+// Variable = We use variables to store values so they can be reused throughout the program.
+// Value = A value is a specific piece of data, such as a number, a character, or a Boolean, that can be stored in a variable.
+console.log(myName); // It will displa/print Mujaahid in the console inside the dev tools.
+
+// A variable name can contain: Letters: a–z, A–Z, Digits: 0–9 (but not as the first character), Underscores: _, Dollar signs: $ other than that not allowed.
+
+// Make your variables descriptive for ex
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher"; // this one is better and recommended.
+
+let job1 = "Programmer";
+let job2 = "Teacher"; // this is allowed but it's not a good practice and not descriptive.
